81f663ba531b2733339a1af73b44ef2aacc1614e
Ana Escarzaga <ana.escarzaga.medina@gmail.com>
2019-10-11 12:32:53 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+secret = 77
+
+guess = int(input("Guess the secret number: "))
+if guess == secret:
+    print("Congratulations, you guessed the secret number!")
+else:
+    print("Sorry, wrong number!")
+    print("Try again")
+
+
+
